2167735a7d2bc443f63d2d0266ed84537d5c80ac
hardycathal <hardyfcathal@gmail.com>
2025-01-08 14:44:14 +0000

    Created new menu micro service

---
+++ src/main/java/ie/atu/projectch2/ProjectCh2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.projectch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCh2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/projectch2/ProjectCh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
