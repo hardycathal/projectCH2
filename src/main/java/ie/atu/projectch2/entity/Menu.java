59a454e8562fb6b56e8b2fca56611f6118749279
hardycathal <hardyfcathal@gmail.com>
2025-01-08 14:52:50 +0000

    Added menu entity

---
+++ src/main/java/ie/atu/projectch2/entity/Menu.java
@@ -0,0 +1,74 @@
+package ie.atu.projectch2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private String category;
+    @NotNull
+    private Double price;
+
+    public Menu() {
+    }
+
+    public Menu(Long id, Double price, String category, String description, String name) {
+        this.id = id;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
